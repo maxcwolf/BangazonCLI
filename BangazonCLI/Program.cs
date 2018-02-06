905bafcd8fc9fd205892a588955720936867a0b2
Chase Steely <chasesteely@gmail.com>
2018-02-06 11:11:22 -0600

    restructure

---
+++ BangazonCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BangazonCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
