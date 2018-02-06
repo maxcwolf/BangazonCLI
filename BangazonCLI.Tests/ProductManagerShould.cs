ea5fafde9d15db1b939309e9af206c9910ed3ead
Ray Medrano <rmbw74@gmail.com>
2018-02-06 14:02:29 -0600

    added model and manager folders and files

--- BangazonCLI.Tests/ProductManagerShould.cs
+++ BangazonCLI.Tests/ProductManagerShould.cs
@@ -1,43 +1,25 @@
 using System;
+using BangazonCLI.Managers;
+using BangazonCLI.Models;
 using Xunit;
-using BangazonCLI;
-using System.Collections.Generic;
 
 namespace BangazonCLI.Tests
 {
     public class ProductManagerShould
     {
-        private Product _product;
-        public ProductManagerShould()
-        {
-            /*
-                Properties of Product
-                    - <int> Id
-                    - <int> CustomerId (foreign key)
-                    - <string> Title
-                    - <string> Description
-                    - <int> Price
-                    - <int> Quantity
-                    - <datetime> DateAdded
-             */
-             _product = new Product(
-                 1,
-                 1,
-                 "Plumbus",
-                 "Travel Sized Plumbus",
-                 1500,
-                 1,
-                 "2018-02-07"
-             );
-        }
 
         [Fact]
         public void AddProductToProductTable()
         {
-            ProductManager prodManager = new ProductManager();
-            Product NewProduct = _product;
-            ProdManager.AddProductToCustomer(_product);
-            Assert.Contains(NewProduct, ProdManger.getActiveCustomerProducts);
+
+            ProductManager pm = new ProductManager();
+
+            Product NewProduct = new Product();
+
+            pm.AddProduct(NewProduct);
+
+
+            Assert.Contains(NewProduct, pm.GetCustomerProducts);
         }
     }
 }
