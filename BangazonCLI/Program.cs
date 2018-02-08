fdc6aa1e7f64e7a8570f37301de4af17b2f57e27
Christopher Miller <christophermiller@Christophers-MacBook-Pro.local>
2018-02-08 10:46:56 -0600

    removed unneccessary db reference

--- BangazonCLI/Program.cs
+++ BangazonCLI/Program.cs
@@ -1,7 +1,6 @@
 ﻿//Author: Max Wolf
 //Purpose: Entry point to the command line interface menus
 using System;
-using BangazonCLI.Data;
 using BangazonCLI.Menus;
 
 namespace BangazonCLI
@@ -10,11 +9,8 @@ namespace BangazonCLI
     {
         static void Main(string[] args)
         {
-            DatabaseInterface db = new DatabaseInterface();
-            //Show the main menu on load
-            
+            //Show the main menu on load   
             MainMenu.Show();
-
 
         }
     }
