d4feb31aa62c335f3490b932fde5dbdc6e9a976f
Ray Medrano <rmbw74@gmail.com>
2018-02-09 13:35:23 -0600

    Modified constructor so that datetime is now a string as that is what will be retrned from the database

--- BangazonCLI/Models/Product.cs
+++ BangazonCLI/Models/Product.cs
@@ -10,18 +10,18 @@ namespace BangazonCLI.Models
         public string Description {get; set;} // Product Description
         public int Price {get; set;} // Product price stored as integer i.e. $19.95 = 1995
         public int Quantity {get; set;}//Product Quantity
-        public DateTime DateAdded {get; set;} // Date product was added to system (calculated at creation)
+        public String DateAdded {get; set;} // Date product was added to system this is calculated by the database and returned as a string
 
 
 
         //This is the Default Product constructor that takes 6 arguments as defined above
         // Id, CustomerId, Title, Description, Price, Quantity
 
-        public Product(int id, int customerId, string title, string description, int price, int quantity, DateTime dateAdded)
+        public Product(int id, int customerId, string title, string description, int price, int quantity, string dateAdded)
         {
             this.Id = id;
             this.CustomerId = customerId;
-            this.Title = title;
+            this.Title = title; // product title
             this.Description = description;
             this.Price = price;
             this.Quantity = quantity;
