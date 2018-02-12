cde771882957764c405268e6c0fe36254cb9d0d2
Ray Medrano <rmbw74@gmail.com>
2018-02-12 13:00:56 -0600

    completed remove product functionality

--- BangazonCLI/Menus/RemoveProductMenu.cs
+++ BangazonCLI/Menus/RemoveProductMenu.cs
@@ -23,12 +23,37 @@ namespace BangazonCLI
       Console.Clear();
       Console.WriteLine ("CHOOSE A PRODUCT TO REMOVE");
       Console.WriteLine ("**************************");
+      Console.WriteLine();
       //Add a line for each product the customer has
       CustomerProductList.ForEach(p =>
         {
-            Console.WriteLine($"{CustomerProductList.IndexOf(p)+1}. {p.Title} {p.Description}");
+            Console.WriteLine($"{CustomerProductList.IndexOf(p)+1}. {p.Title}:   {p.Description}");
         }
       );
+      Console.WriteLine("Enter 'Q' to Return To Main Menu");
+      Console.Write("> ");
+      var Result = Console.ReadLine();
+
+      //if user enters Q, they are returned to the main menu, otherwise product selected
+      if (Result.ToLower() == "q")
+        {
+            FeatureMenu.Show(ActiveCustomerId);
+        }
+        else
+        {
+            //extract product Id from the selected product
+            Product SelectedProduct = CustomerProductList[int.Parse(Result)-1];
+
+            //Delete Product If Not on Order
+            Console.Clear();
+            pm.DeleteCustomerProduct(SelectedProduct.Id);
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key To Continue");
+            Console.Write("> ");
+            Console.ReadKey();
+            Console.Clear();
+            RemoveProductMenu.Show(ActiveCustomerId);
+        }
 
     }
   }
